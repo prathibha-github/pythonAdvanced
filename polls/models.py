28778cbbf4998e23e9186411da3103642d998a63
Prathibha Alam <prathibha.alam@gmail.com>
2012-08-22 14:59:27 -0700

    Committing  changes so far

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=128)
+    pub_date = models.DateTimeField(verbose_name='Date Published')
+    
+    def __unicode__(self):
+        return self.question
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, null=True)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.choice_text + " with " + self.votes +" votes!"
